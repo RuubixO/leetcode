a62a4e3f6f02d6fb4d67cb280f6de92bd244f5cc
Ben <ruubixo@gmail.com>
2019-04-03 14:47:29 -0400

    Added directory and folder structure for tests in leetcode repo. Added .gitignore file. Restructured jewels_and_stones directory.

---
+++ tests/easy/test_jewels_and_stones/test_are_jewels_present.py
@@ -0,0 +1,17 @@
+import jewels_and_stones
+import pytest
+
+
+def test_how_many_jewels_in_stones():
+    J = "aA"
+    S = "aAAbbbb"
+
+    assert jewels_and_stones.numJewelsInStones(self, J, S) == 3
+
+
+def test_uppercase_true():
+    assert val.isupper()
+
+
+def test_uppercase_false():
+    assert val.islower()
